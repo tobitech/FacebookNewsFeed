a71fc08ec35c61b2436e3775af3202c53ec81009
Tobi Omotayo <tobitech@ymail.com>
2017-01-01 22:08:38 +0100

    setup the project and display static content data on the feed controller

---
+++ MyFacebookFeed/FeedCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCell.swift
+//  MyFacebookFeed
+//
+//  Created by Tobi Omotayo on 01/01/2017.
+//  Copyright © 2017 Tobi Omotayo. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
---
+++ MyFacebookFeed/FeedController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  MyFacebookFeed
+//
+//  Created by Tobi Omotayo on 01/01/2017.
+//  Copyright © 2017 Tobi Omotayo. All rights reserved.
+//
+
+import UIKit
+
+let cellId = "cellId"
+
+class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1.0)
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 300)
+    }
+
+}
+
