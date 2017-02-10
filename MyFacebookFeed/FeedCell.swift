76ff99601aea8674cd595277142d517bced25e4b
Tobi Omotayo <tobitech@ymail.com>
2017-02-10 10:24:23 +0100

    restructure the app with a new Posts Model and add new images

--- MyFacebookFeed/FeedCell.swift
+++ MyFacebookFeed/FeedCell.swift
@@ -18,13 +18,31 @@ class FeedCell: UICollectionViewCell {
     @IBOutlet weak var statusTextView: UITextView!
     @IBOutlet weak var postImageView: UIImageView!
     
+    var post: Post? {
+        didSet {
+            
+        }
+    }
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
         likeButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 0)
         commentButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 0)
         shareButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 0)
+    }
+    
+    func configureCell() {
+        nameLabel.text = post?.name
+        statusTextView.text = post?.statusText
         
+        if let profileImageName = post?.profileImageName {
+            profileImageView.image = UIImage(named: profileImageName)
+        }
+        if let statusImageName = post?.statusImageName {
+            postImageView.image = UIImage(named: statusImageName)
+        }
     }
 
 }
--- MyFacebookFeed/FeedController.swift
+++ MyFacebookFeed/FeedController.swift
@@ -10,82 +10,33 @@ import UIKit
 
 let cellId = "cellId"
 
-class Post {
-    var name: String?
-    var status: String?
-    var profileImageName: String?
-    var statusImageName: String?
-    var numLikes: Int?
-    var numComments: Int?
-}
+let posts = Posts()
 
 class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
-
-    var posts = [Post]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let postMark = Post()
-        postMark.name = "Mark Zuckerberg"
-        postMark.profileImageName = "zuckprofile"
-        postMark.status = "By giving people the power to share, we're making the world more transparent."
-        postMark.statusImageName = "zuckdog"
-        postMark.numLikes = 400
-        postMark.numComments = 123
-        
-        let postSteve = Post()
-        postSteve.name = "Steve Jobs"
-        postSteve.profileImageName = "steve_profile"
-        postSteve.status = "Design is not just what it looks like and feels like. Design is how it works.\n\n" +
-            "Being the richest man in the cemetery doesn't matter to me. Going to bed at night saying we've done something wonderful, that's what matters to me.\n\n" +
-        "Sometimes when you innovate, you make mistakes. It is best to admit them quickly, and get on with improving your other innovations."
-        postSteve.statusImageName = "steve_status"
-        postSteve.numLikes = 1000
-        postSteve.numComments = 55
-        
-        let postGandhi = Post()
-        postGandhi.name = "Mahatma Gandhi"
-        postGandhi.profileImageName = "gandhi"
-        postGandhi.status = "Live as if you were to die tomorrow; learn as if you were to live forever.\n" +
-            "The weak can never forgive. Forgiveness is the attribute of the strong.\n" +
-        "Happiness is when what you think, what you say, and what you do are in harmony."
-        postGandhi.statusImageName = "gandhi_status"
-        postGandhi.numLikes = 333
-        postGandhi.numComments = 22
-        
-        
-        posts.append(postMark)
-        posts.append(postSteve)
-        posts.append(postGandhi)
        
         collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1.0)
         
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return posts.count
+        return posts.numberOfPosts()
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let feedCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! FeedCell
-        let post = posts[indexPath.row]
-        feedCell.nameLabel.text = post.name
-        feedCell.statusTextView.text = post.status
         
-        if let profileImageName = post.profileImageName {
-            feedCell.profileImageView.image = UIImage(named: profileImageName)
-        }
-        if let statusImageName = post.statusImageName {
-            feedCell.postImageView.image = UIImage(named: statusImageName)
-        }
+        feedCell.post = posts[indexPath]
+        feedCell.configureCell()
         
         return feedCell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        if let statusText = posts[indexPath.row].status {
+        if let statusText = posts[indexPath].statusText {
             
             let rect = NSString(string: statusText).boundingRect(with: CGSize(width: view.frame.width, height: 1000), options: NSStringDrawingOptions.usesFontLeading.union(NSStringDrawingOptions.usesLineFragmentOrigin), attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 14)], context: nil)
             
